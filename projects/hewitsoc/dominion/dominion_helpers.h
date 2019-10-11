4170f5d97b402823922edde6370300041e34b339
chewitso <clayton.hewitson@softvision.com>
2019-10-11 14:24:49 -0700

    refactor and bugs - assignment-2

--- projects/hewitsoc/dominion/dominion_helpers.h
+++ projects/hewitsoc/dominion/dominion_helpers.h
@@ -11,5 +11,10 @@ int gainCard(int supplyPos, struct gameState *state, int toFlag, int player);
 int getCost(int cardNumber);
 int cardEffect(int card, int choice1, int choice2, int choice3,
                struct gameState *state, int handPos, int *bonus);
+int baronRefactor(int choice1, int currentPlayer, struct gameState *state);
+int minionRefactor(int choice1, int choice2, int currentPlayer, struct gameState *state, int handPos);
+int ambassadorRefactor(int choice1, int choice2, int currentPlayer, struct gameState *state, int handPos);
+int tributeRefactor(int currentPlayer, int nextPlayer, int tributeRevealedCards[], struct gameState *state);
+int mineRefactor(int choice1, int choice2, int currentPlayer, struct gameState *state, int handPos);
 
 #endif
